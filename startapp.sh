760329711b87ca2e23958d25d5eacb0aaa192555
David D. <i_robot73@hotmail.com>
2024-12-06 14:37:19 -0500

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -15,9 +15,12 @@ mkdir -p $HOME/.wine
 
 # If .INI doesn't exist, create one so the window doesn't minimize
 # https://mamedev.emulab.it/clrmamepro/#docs
-if [ ! -e /opt/clrmamepro/cmpro.ini ]; then
-    printf "[CMPRO SETTINGS]\nAdv_HideWindow = off" >> /opt/clrmamepro/cmpro.ini
-fi
+#
+# Moved to Dockerfile.  User should BIND so .INI persists
+#
+#if [ ! -e /opt/clrmamepro/cmpro.ini ]; then
+#    printf "[CMPRO SETTINGS]\nAdv_HideWindow = off" > /opt/clrmamepro/cmpro.ini
+#fi
 
 # Launch clrmamepro
 wine /opt/clrmamepro/cmpro64.exe 2>&1 | awk -W Interactive '{print "[clrmamepro] " $0}'
